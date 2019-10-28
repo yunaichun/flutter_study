d17edcfb895e958f519092e8e25bfbbaaa48a913
yunaichun <yunaichun@qingting.fm>
2019-10-28 10:07:58 +0800

    重新组织项目目录

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,36 @@
 import 'package:flutter/material.dart';
-// import '01/HellowWorld.dart';
-// import '02/TextWidget.dart';
-// import '02/Container.dart';
-// import '02/Image.dart';
-// import '02/ListView.dart';
-// import '02/ListView.horizontal.dart';
-// import '02/ListView.builder.dart';
-// import '02/GridView.dart';
-// import '03/Row.dart';
-// import '03/Column.dart';
-// import '03/Stack.dart';
-// import '03/Stack.Positioned.dart';
-// import '03/Column.ListTile.dart';
-// import '04/Navigator.dart';
-// import '04/Navigator.push.dart';
-// import '04/Navigator.pop.dart';
-// import '04/Image.asset.dart';
-// import '05/01/BottomNavigationBar.dart';
-// import '05/02/FloatingActionButton.dart';
-// import '05/03/CustomRoute.dart';
-// import '05/04/BackdropFilter.dart';
-// import '05/05/TabBar.dart';
-// import '05/06/searchDelegate.dart';
-// import '05/07/Wrap.dart';
-// import '05/08/Expansion.dart';
-// import '05/09/ClipPath.dart';
-// import '05/10/AnimationController.dart';
-// import '05/11/CupertinoPageRoute.dart';
-// import '05/12/Tooltip.dart';
-import '05/13/Draggable.dart';
+
+/*以下是 demo 页面*/
+// import 'demo/01/HellowWorld.dart';
+// import 'demo/02/TextWidget.dart';
+// import 'demo/02/Container.dart';
+// import 'demo/02/Image.dart';
+// import 'demo/02/ListView.dart';
+// import 'demo/02/ListView.horizontal.dart';
+// import 'demo/02/ListView.builder.dart';
+// import 'demo/02/GridView.dart';
+// import 'demo/03/Row.dart';
+// import 'demo/03/Column.dart';
+// import 'demo/03/Stack.dart';
+// import 'demo/03/Stack.Positioned.dart'
+// import 'demo/03/Column.ListTile.dart';
+// import 'demo/04/Navigator.dart';
+// import 'demo/04/Navigator.push.dart';
+// import 'demo/04/Navigator.pop.dart';
+// import 'demo/04/Image.asset.dart';
+// import 'demo/05/01/BottomNavigationBar.dart';
+// import 'demo/05/02/FloatingActionButton.dart';
+// import 'demo/05/03/CustomRoute.dart';
+// import 'demo/05/04/BackdropFilter.dart';
+// import 'demo/05/05/TabBar.dart';
+// import 'demo/05/06/searchDelegate.dart';
+// import 'demo/05/07/Wrap.dart';
+// import 'demo/05/08/Expansion.dart';
+// import 'demo/05/09/ClipPath.dart';
+// import 'demo/05/10/AnimationController.dart';
+// import 'demo/05/11/CupertinoPageRoute.dart';
+// import 'demo/05/12/Tooltip.dart';
+import 'demo/05/13/Draggable.dart';
 
 // 主入口函数
 void main() => runApp(MyApp());
